d32c72e5feb422e4650e3ec7ead8a09d44238089
xyDu <xy_du@qq.com>
2020-10-13 16:27:36 +0800

    initial project set up

---
+++ src/main/java/dxy/springframework/adspringrecipeapp/AdSpringRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package dxy.springframework.adspringrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSpringRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdSpringRecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dxy/springframework/adspringrecipeapp/AdSpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dxy.springframework.adspringrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdSpringRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
