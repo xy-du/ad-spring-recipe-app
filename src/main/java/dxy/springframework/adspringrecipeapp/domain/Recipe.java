2b317fb02d02e7cd8a725ddbbef112168430afc6
xyDu <xy_du@qq.com>
2020-10-16 19:19:38 +0800

    create ingredient and one-to-many relation to Recipe.

--- src/main/java/dxy/springframework/adspringrecipeapp/domain/Recipe.java
+++ src/main/java/dxy/springframework/adspringrecipeapp/domain/Recipe.java
@@ -22,6 +22,9 @@ public class Recipe {
     //todo add
     //private Difficulty difficulty;
 
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
+    private Ingredient ingredient;
+
     @Lob
     private Byte[] image;
 
