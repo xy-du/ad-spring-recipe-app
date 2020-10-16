22dff5ab9de29b251a60ef05c3c3c4d474b4b3ab
xyDu <xy_du@qq.com>
2020-10-16 17:57:31 +0800

    added Note and Recipe entity

---
+++ src/main/java/dxy/springframework/adspringrecipeapp/domain/Note.java
@@ -0,0 +1,43 @@
+package dxy.springframework.adspringrecipeapp.domain;
+
+import javax.persistence.*;
+
+/**
+ * @author AD
+ * @date 2020/10/16
+ */
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNote() {
+        return recipeNote;
+    }
+
+    public void setRecipeNote(String recipeNote) {
+        this.recipeNote = recipeNote;
+    }
+}
