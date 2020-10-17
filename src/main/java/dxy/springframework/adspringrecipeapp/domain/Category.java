2ee55c3ee47ea56651d2f6246fed3927b290f6ba
xyDu <xy_du@qq.com>
2020-10-17 11:23:46 +0800

    added Category Entity and relate it to Recipe.

---
+++ src/main/java/dxy/springframework/adspringrecipeapp/domain/Category.java
@@ -0,0 +1,43 @@
+package dxy.springframework.adspringrecipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * @author AD
+ * @date 2020/10/17
+ */
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
--- src/main/java/dxy/springframework/adspringrecipeapp/domain/Recipe.java
+++ src/main/java/dxy/springframework/adspringrecipeapp/domain/Recipe.java
@@ -33,6 +33,12 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
