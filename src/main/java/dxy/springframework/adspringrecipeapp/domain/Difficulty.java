b4a6c8606532f934f7ee30af65d53bd68dba57ae
xyDu <xy_du@qq.com>
2020-10-17 10:34:20 +0800

    add Difficulty enum and update Recipe

---
+++ src/main/java/dxy/springframework/adspringrecipeapp/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package dxy.springframework.adspringrecipeapp.domain;
+
+/**
+ * @author AD
+ * @date 2020/10/17
+ */
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/dxy/springframework/adspringrecipeapp/domain/Recipe.java
+++ src/main/java/dxy/springframework/adspringrecipeapp/domain/Recipe.java
@@ -19,14 +19,15 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
     private Ingredient ingredient;
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
@@ -101,5 +102,29 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
     }
 }
